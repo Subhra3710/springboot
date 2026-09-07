572a2903ac4e019395e1ce86baca8e86952649f8
Subhra3710 <sahoosubhra2007@gmail.com>
2026-09-07 20:24:22 +0530

    Expense jpa

--- expense-tracker/src/main/java/com/subhra/expense_tracker/ExpenseController.java
+++ expense-tracker/src/main/java/com/subhra/expense_tracker/ExpenseController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -55,15 +59,17 @@ public class ExpenseController {
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Expense>(Expense.class));
 
         }
-        @RequestMapping(value= "/expenses/{id}", method = RequestMethod.GET)
-        public Expense getExpensById(@PathVariable int id){
+       // @RequestMapping(value= "/expenses/{id}", method = RequestMethod.GET)
+        @GetMapping("/expenses/{id}")
+        public Expense getExpenseById(@PathVariable int id){
            // System.out.println("ID is:" + id);    
            var sql = "SELECT * FROM %s WHERE id=?".formatted(EXPENSES_TABLE);
            Expense expense = jdbcTemplate.queryForObject
                                 (sql, new BeanPropertyRowMapper<>(Expense.class), id);
            return expense;
         }
-        @RequestMapping(value= "/expenses" , method = RequestMethod.POST)
+        //@RequestMapping(value= "/expenses" , method = RequestMethod.POST)
+        @PostMapping("/expenses/{id}")
         public Expense creatExpense(@RequestBody Expense expense){
             var sql = "INSERT INTO %s (title, category, price, date) VALUES(?,?,?,?)"
                                                             .formatted(EXPENSES_TABLE);
@@ -71,5 +77,23 @@ public class ExpenseController {
                                                 expense.getPrice(), expense.getDate());
             return expense;
         }
-    }  
+
+
+        // @RequestMapping(value= "/expenses" , method = RequestMethod.DELETE)
+        @DeleteMapping("/expenses/{id}")
+            public void deleteExpense(@PathVariable int id){
+                String sql = "DELETE FROM %s WHERE id=?".formatted(EXPENSES_TABLE);
+                jdbcTemplate.update(sql, id);
+            }
+
+        @PutMapping("/expenses")
+            public Expense updateExpense(@RequestBody Expense expense){
+                var sql ="UPDATE %s SET title=?, category=?, price=?, date=? WHERE id=?".formatted(EXPENSES_TABLE);
+                jdbcTemplate.update(sql, expense.getTitle(), expense.getCategory(), expense.getPrice(), expense.getDate(), expense.getId());
+                return getExpenseById(expense.getId());    
+            }
+        
+        }
+            
+    
 
